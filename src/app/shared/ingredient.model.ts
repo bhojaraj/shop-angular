fe03f548e5f088afafef14860d499ad4ac2ff0b8
Bharath <bharathgdaba@gmail.com>
2021-03-15 18:24:33 +0530

    Adding basics of the app

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
+
+// above is the shortcut for below code
+// export class Ingredient {
+//     public name: string;
+//     public amount: number;
+
+//     constructor(name: string, amount: number) {
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
